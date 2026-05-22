755d9ee66bae11457e7ad54327d1271856354148
Bahati Philemon <bahatiphilmon8@gmail.com>
2026-05-22 15:20:58 +0200

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+const App = () => {
+  return (
+    <>
+    
+      <div>
+          <h1 className='bg-white text-green-500'>Welcome to my React App!</h1>
+      </div>
+      
+    </>
+  );
+};
+
+export default App;
